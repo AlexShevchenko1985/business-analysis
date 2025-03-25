41faa916997c1058c12423be3361d74b7c73ed36
AlexShevchenko1985 <alexdoc1985@gmail.com>
2025-03-25 14:25:04 +0100

    single services page

---
+++ templates/single-services.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Template name: Services Single
+ */
+get_header();
+?>
+<section id="block-developersforagenciesandinhouseteams" class="block hero-section">
+     <div class="container-lg">
+	    <div class="block__content row">
+	        <div class="hero-text col-lg-6">
+				<h1 class="block__title">Eliciting requirements services — Streamline project success</h1>
+	            <div class="text-content clearfix field field--name-body field--type-text-with-summary field--label-hidden field__item">
+	                <p>uccessful projects start with crystal-clear requirements. Our expert business analysts dive deep into understanding your goals, collaborating closely with stakeholders to uncover, validate, and prioritize every essential detail through <strong>Eliciting Requirements service</strong>. The result? A clear, actionable roadmap that guides your team, minimizes missteps and keeps scope creep away.</p>
+	            </div>
+	        </div>
+			<div class="hero-image col-lg-6">
+	            <div class="field field--name-field-hero-image field--type-entity-reference field--label-hidden field__item">
+	                <div class="media media--type-image media--view-mode-hero-image">
+	                    <div class="paragraph-image-item">
+	                        <img width="584" height="640" src="<?php echo get_template_directory_uri() ?>/src/images/services-single-hero.webp" class="attachment-large size-large" alt="">
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	 </div>
+</section>
+<section class="services single-services" style="">
+    <div class="container-lg">
+        <div class="cards-holder">
+            <div class="item transparent-item">
+                <!--<div class="sup-title"></div>-->
+                <div class="title">Here’s how we do requirement elicitation:</div>
+                <!--<p></p>-->
+            </div>
+			<div class="item">
+                <div class="heading">
+                    <img src="https://business-analysis.test/wp-content/uploads/2025/03/s-icon1.svg" class="attachment- size-" alt="" decoding="async">
+                    <h4>Conduct stakeholder interviews and workshops:</h4>
+                </div>
+                <p>Our experts engage with key stakeholders to uncover insights, align expectations, and build a shared vision.</p>
+				<!--<a href="#" target="_self" class="arrow-btn">View more details  <img src="/wp-content/themes/business-analysis/src/images/arrow-r.svg" alt="Arrow"></a>-->
+            </div>
+            <div class="item">
+                <div class="heading">
+                    <img src="https://business-analysis.test/wp-content/uploads/2025/03/s-icon2.svg" class="attachment- size-" alt="" decoding="async">
+                    <h4>Document business and technical requirements:</h4>
+                </div>
+                <p>Business analysts ensure every detail is captured with precision for seamless communication and execution.</p>
+                <!--<a href="#" target="_self" class="arrow-btn">View more details  <img src="/wp-content/themes/business-analysis/src/images/arrow-r.svg" alt="Arrow"></a>-->
+            </div>
+            <div class="item">
+                <div class="heading">
+                    <img src="https://business-analysis.test/wp-content/uploads/2025/03/s-icon3.svg" class="attachment- size-" alt="" decoding="async">
+                    <h4>Create and manage requirements traceability matrices:</h4>
+                </div>
+                <p>We maintain alignment and clarity by tracking requirements throughout the project lifecycle.</p>
+                <!--<a href="#" target="_self" class="arrow-btn">View more details  <img src="/wp-content/themes/business-analysis/src/images/arrow-r.svg" alt="Arrow"></a>-->
+            </div>
+        </div>
+        <div class="text-ribbon">
+            <p><strong>Ready to lay the groundwork for your next big win?</strong></p>
+            <p>Let’s turn your vision into a roadmap for success.</p>
+        </div>
+
+        <div class="btn-holder">
+            <a href="https://business-analysis.test/" class="button-cta secondary-btn">Get your project roadmap now!</a>
+        </div>
+
+    </div>
+</section>
+
+<section class="list-with-numbers services-single" style="background: rgba(232, 232, 232, 0.7);">
+    <div class="container-lg">
+        <div class="heading">
+            <!--<div class="sup-title"></div>-->
+            <h2>Our requirement elicitation process</h2>
+            <p>Please take a closer look at our BA approach to eliciting requirements for the success of your business project.</p>
+        </div>
+		<div class="list-with-numbers-grid">
+            <div class="item">
+                <div class="heading">
+                    <div class="number">1</div>
+                    <h4>01 - Interviewing stakeholders</h4>
+                </div>
+                <p>Our expert Business Analysts begin by engaging key stakeholders (team members, decision-makers, and end-users) to understand their goals, challenges, and expectations. This step uncovers valuable insights and sets the stage for clear communication and mutual understanding.</p>
+            </div>
+            <div class="item">
+                <div class="heading">
+                    <div class="number">2</div>
+                    <h4>02 - Gathering requirements</h4>
+                </div>
+                <p>Next, we collect and consolidate information from various sources, including stakeholder input, existing documentation, industry research, and system analysis. This ensures no critical detail is overlooked and provides a comprehensive view of what’s needed.</p>
+            </div>
+            <div class="item">
+                <div class="heading">
+                    <div class="number">3</div>
+                    <h4>03 - Documenting requirements</h4>
+                </div>
+                <p>Drupfan BA team meticulously organizes and records all requirements into clear, accessible documents. This step ensures transparency and creates a solid roadmap for developers during the implementation phase.</p>
+            </div>
+            <div class="item">
+                <div class="heading">
+                    <div class="number">4</div>
+                    <h4>04 - Creating measurable matrices</h4>
+                </div>
+                <p>Our specialists in Business Analysis develop measurable matrices to validate requirements and track their progress. This ensures every requirement aligns with project goals and is clearly defined, so it can be measured and managed effectively.</p>
+            </div>
+            <div class="item">
+                <div class="heading">
+                    <div class="number">5</div>
+                    <h4>05 - Developing a traceable roadmap</h4>
+                </div>
+                <p>Finally, Drupfan seasoned BAs build a step-by-step roadmap that connects each requirement to specific project objectives. This roadmap acts as a guide during the project development.</p>
+            </div>
+        </div>
+		<!--<div class="arrow-block">
+            <div class="descr"></div>
+        </div>-->
+
+        <!--<div class="btn-holder">
+            <a href="javascript:void(0)" class="button-cta webform-dialog">Hire business analyst</a>
+        </div>-->
+
+    </div>
+</section>
+<section class="faq-section services-single" style="background: #ffffff;">
+    <div class="container-lg">
+        <div class="heading">
+            <!--<div class="sup-title">FAQ</div>-->
+            <h2>Frequently asked questions:<br> Requirements elicitation</h2>
+        </div>
+		<div class="faq-holder">
+            <div class="faq-item">
+                <div class="faq-title">When do I need business analysis?</div>
+                <div class="faq-body" style="height: 0px;">
+                    <p>Drupfan proudly collaborates with businesses of any size: from startups to global enterprises. With headquarters in Ottawa, Canada, we offer flexible engagement models for our clients from Northern America and Europe.</p>
+                </div>
+            </div>
+            <div class="faq-item">
+                <div class="faq-title">What types of requirements do I need to elicit to start my business?</div>
+                <div class="faq-body" style="height: 0px;">
+                    <p>The key types of requirements vital for successful project implementation are:</p>
+                    <ul>
+                        <li><strong>Functional requirements</strong></li>
+                    </ul>
+                    <p>By eliciting functional requirements, BAs determine what your project needs in order to achieve its goals and objectives. This is a set of specific tasks or operations a system (product) must perform.</p>
+                </div>
+            </div>
+            <p>Requirements eliciting is the process of discovering, analyzing, and processing information that is not readily available. Requirement elicitation requires expertise, the usage of a special toolset, and extensive knowledge of business analysis.</p>
+        </div>
+	</div>
+</section>
+<?php
+get_footer();
